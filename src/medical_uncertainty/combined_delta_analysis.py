75242e3fba3f8ee943f1e1eefd1ff837bf4b227c
aparnaelangovan <aparnaelangovan@gmail.com>
2026-03-23 21:47:37 -0700

    Add accuracy range for model

--- src/medical_uncertainty/combined_delta_analysis.py
+++ src/medical_uncertainty/combined_delta_analysis.py
@@ -30,6 +30,80 @@ models = [
      Path(__file__).parent.parent.parent / "data_dir/checkpoints/gpt-5.1/gpt-5.1_scores_exclude_small_samples.json")
 ]
 
+def create_boxplot_by_pd_df(metric_label, df_model, column_name_model,df_h = None, column_name_human=None):
+    """Create plot showing all three models together for accuracy"""
+    fig, ax = plt.subplots(figsize=(20, 10))
+
+
+    pvalues = []
+
+    # Group by PA level and calculate averages
+    all_data = []
+    all_data_props = []
+    # Define median properties with a specific color and linewidth
+    median_properties = dict(color='black', linewidth=2.5)
+    for pa_level in pd_levels:
+        #if pa_level == "All": continue
+        print(pa_level)
+        pa_data = df_model[df_model['PA'] == pa_level]
+        score_values = pa_data[column_name_model]
+
+        all_data.append(score_values)
+        all_data_props.append(
+            {"pa_level": pa_level,
+             "type": "M", # model
+             "hatch": None
+             }
+
+        )
+    if df_h is not None:
+        for pa_level in pd_levels:
+
+            # add human
+            all_data.append(df_h[df_h['PA'] == pa_level][column_name_human])
+            all_data_props.append(
+                {"pa_level": pa_level,
+                 "type": "H",  # model
+                 "hatch": "."
+                 }
+
+            )
+
+
+
+
+
+
+
+
+
+    ax.set_ylim(0.0, 1.01)
+
+
+    # # Plot with model-specific marker
+    bplot = ax.boxplot(all_data, medianprops=median_properties, showmeans=True, patch_artist=True)
+    for i in range(len(all_data)):
+        box = bplot['boxes'][i]
+
+
+        box.set(facecolor= color_map[all_data_props[i]["pa_level"]], alpha=0.7)
+        # change hatch
+        if all_data_props[i]["hatch"]:
+            box.set(hatch=all_data_props[i]["hatch"])
+
+
+    # Add horizontal line at y=0 (no difference)
+    ax.axhline(y=0, color='black', linestyle='--', alpha=0.5, label='No difference')
+
+
+    ax.set_xticklabels([f"{prop['type']} $p_d{pd_levels_symbol_maps.get(prop['pa_level'], '\\rightarrow')}${prop['pa_level']}"
+                   for prop in all_data_props])
+    ax.set_ylabel(f'Δ {metric_label}', fontsize=20, fontweight='bold')
+
+    ax.grid(True, alpha=0.3)
+
+    plt.tight_layout()
+    return fig
 
 def load_data(json_path):
     """Load data from JSON checkpoint"""
@@ -151,6 +225,26 @@ def create_paired_comparer_boxplot_by_df(df, column_name_1, column_name_2, metri
     return fig
 
 
+def create_boxplot_by_pd_df_all_models(metric_label, column_model_name,column_human_name=None  ):
+    dfs = []
+    for i, (model_name, json_path) in enumerate(models):
+        df_m = load_data(json_path)
+        df_m["model"] = model_name
+        # df_m["model_marker"] =  [model_name]
+        dfs.append(df_m)
+
+    df = pd.concat(dfs)
+
+    # just use anyone as human performance is the same.
+    df_h= None
+    if column_human_name:
+        human_data = models[0][1]
+
+        df_h = load_data(human_data)
+
+    return create_boxplot_by_pd_df(metric_label, df, column_model_name, df_h, column_human_name )
+
+
 def create_multimodel_mean_variance_pd_comparer_boxplot_by_df(column_name, metric_label):
     dfs = []
     for i, (model_name, json_path) in enumerate(models):
@@ -163,6 +257,10 @@ def create_multimodel_mean_variance_pd_comparer_boxplot_by_df(column_name, metri
 
     return create_mean_variance_pd_comparer_boxplot_by_df(df, column_name, metric_label)
 
+
+def create_multimodel_pd_boxplot_by_df_acc():
+
+    return create_boxplot_by_pd_df_all_models("Accuracy","model_accuracy",None )
 
 
 def create_multimodel_mean_variance_pd_delta_h_vs_allmodels_f1_comparer_boxplot():
@@ -188,6 +286,13 @@ def main():
     fig.savefig(output_dir / "combined_simple_boxplot_delte_accuracy_analysis.png", dpi=400, bbox_inches='tight')
     plt.close(fig)
     print("Combined simple boxplot Delta accuracy Analysis visualization saved")
+
+
+
+    fig = create_multimodel_pd_boxplot_by_df_acc()
+    fig.savefig(output_dir / "multimodel_pd_boxplot_acc.png", dpi=400, bbox_inches='tight')
+    plt.close(fig)
+    print("multimodel_pd_boxplot acc saved")
 
 
 if __name__ == "__main__":
